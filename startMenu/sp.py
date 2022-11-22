61513eb028c3a7363658bf547cba415bf43e2bd4
Aleksei <you@example.com>
2022-11-22 01:23:39 -0500

    Add start menu package

---
+++ startMenu/sp.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import ttk
+
+LARGEFONT = ("Verdana", 35)
+
+class StartPage(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+
+        self.pack(side = "top", fill = "both", expand = True)
+        self.grid_rowconfigure(0, weight = 1)
+        self.grid_columnconfigure(0, weight = 1)
+
+        label = ttk.Label(self, text ="Admin Dashboard", font = LARGEFONT)
+        label.grid(row = 0, column = 0)
+
+        menu_frame = ttk.Label(self)
+        menu_frame.grid(row = 1, column = 0)
+
+        button1 = ttk.Button(menu_frame, text ="Organizations", command = lambda : parent.show_frame('Organizations'))
+        button1.grid(row = 0, column = 0, padx=15, pady=15)
+  
+        button2 = ttk.Button(menu_frame, text ="Courses", command = lambda : parent.show_frame('Courses'))
+        button2.grid(row = 0, column = 1, padx=15, pady=15)
+        
+        button3 = ttk.Button(menu_frame, text = "Mentors", command = lambda : parent.show_frame('Mentors'))
+        button3.grid(row = 1, column = 0, padx=15, pady=15)
